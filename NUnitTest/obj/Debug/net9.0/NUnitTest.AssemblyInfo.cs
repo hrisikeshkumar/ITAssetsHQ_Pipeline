a190c4f35c36637f688a786670869cb443d18c70
hrisikeshkumar <hrisikeshkumar@gmail.com>
2025-09-13 18:30:28 +0530

    ok

--- NUnitTest/obj/Debug/net9.0/NUnitTest.AssemblyInfo.cs
+++ NUnitTest/obj/Debug/net9.0/NUnitTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NUnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c02e69e9dd6e3d22775073f84c95bc918fd29af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d2b666ea508bd15fa6e5107efa22f72663ad9fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("NUnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NUnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
